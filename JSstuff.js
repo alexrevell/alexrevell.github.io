93a997f3d427354ff1903b335efe22e5184f3641
alexrevell <revellalex@gmail.com>
2015-02-09 00:43:28 +1300

    add JS to make button at wrapper headline change colour of background

---
+++ JSstuff.js
@@ -0,0 +1,13 @@
+function set_background() {
+
+  myBody = document.getElementsByTagName("body")[0];
+  myHeader = MyBody[0]
+  myHeader.style.background = #E3CDA4
+
+  myBodyElements = myBody.getElementById("wrapper");
+
+  // get the first item of the wrapper element
+  myWrapper = document.getElementById("wrapper");
+  myIntro = myWrapper.getElementById("intro");
+  myIntro.style.background = "#111111";
+}
